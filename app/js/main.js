e8b140ba1a5e20d5a9ec567fa60a196f2e094787
JustinMKramp <justinkramp.dev@gmail.com>
2021-11-18 01:50:53 -0500

    clean up

--- app/js/main.js
+++ app/js/main.js
@@ -110,21 +110,21 @@ window.onload = function () {
 };
 
 ///////paragraph animations
-ScrollTrigger.matchMedia({
-	// desktop
-	"(min-width: 800px)": function () {
-		// ScrollTriggers will be reverted/killed when the media query doesn't match anymore.
-		let tl = gsap.timeline({
-			scrollTrigger: {
-				trigger: ".myText",
-				scrub: 5,
-				end: "+=500",
-				// pin: true,
-			},
-		});
-		tl.to(".myText", { x: "-15vw" });
-	},
-});
+// ScrollTrigger.matchMedia({
+// 	// desktop
+// 	"(min-width: 800px)": function () {
+// 		// ScrollTriggers will be reverted/killed when the media query doesn't match anymore.
+// 		let tl = gsap.timeline({
+// 			scrollTrigger: {
+// 				trigger: ".myText",
+// 				scrub: 5,
+// 				end: "+=500",
+// 				// pin: true,
+// 			},
+// 		});
+// 		tl.to(".myText", { x: "-15vw" });
+// 	},
+// });
 
 ///////hide/show Nav animation
 let showAnim = gsap
